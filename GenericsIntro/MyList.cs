38a55ce6d7bdfd950fc5455b0b35e3bd92473b98
sueda akın <sueda_akin@hotmail.com>
2021-01-24 22:50:24 +0300

    20 Ocak

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -21,5 +21,13 @@ namespace GenericsIntro
             }
             items[items.Length - 1] = item;
         }
+        public int Length
+        {
+            get{ return items.Length; }
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
     }
 }
