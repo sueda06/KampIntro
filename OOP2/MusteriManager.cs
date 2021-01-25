353051d5364d853ee8e0df612e69bf4da6902efc
sueda akın <sueda_akin@hotmail.com>
2021-01-26 00:51:21 +0300

    23 ocak

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    // iş sınıfları-  business class
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>() {fileLoggerService,databaseLoggerService };
+            
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager,new List<ILoggerService> { new DatabaseLoggerService() });
+            basvuruManager.BasvuruYap(tasitKrediManager, new List<ILoggerService> { fileLoggerService });
+            basvuruManager.BasvuruYap(konutKrediManager, new List<ILoggerService> { databaseLoggerService });
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(),
+                new List<ILoggerService> { new DatabaseLoggerService(),new SmsLoggerService() });
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager,tasitKrediManager };
+
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            Console.ReadLine();
+        }
+    }
+}
